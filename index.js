6cf89842838aa5946040099ab5d4990349a640b4
sbin0819 <sbinha123@gmail.com>
2021-04-25 17:27:42 +0900

    some changed

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+Vue.component('todo-list', {
+  props: ['todo'],
+  template: `<li>{{ todo.text }}</li>`,
+});
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: '안녕하세요 Vue!',
+    title: 'TODO LIST',
+    isShow: false,
+    todos: [
+      { id: 0, text: 'tutorial Vue' },
+      { id: 1, text: 'Learn FP' },
+      { id: 2, text: 'Storybook' },
+      { id: 3, text: 'Clean the house' },
+    ],
+  },
+  methods: {
+    onHandleShow: function () {
+      this.isShow = !this.isShow;
+    },
   },
 });
